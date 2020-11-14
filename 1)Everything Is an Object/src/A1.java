c1799931e2b2135c6608100e14d0087e5a5028ad
gromov-nikita <hromay.n999@gmail.com>
2020-11-14 17:35:28 +0300

    1

--- 1)Everything Is an Object/src/A1.java
+++ 1)Everything Is an Object/src/A1.java
@@ -1,11 +1,9 @@
 public class A1 {
-    int a;
-    char b;
+    private int a;
+    private char b;
     public static void main(String[] args) {
         A1 obj = new A1();
         System.out.println(obj.a);
-        System.out.println("_________");
         System.out.println(obj.b);
-        System.out.println("_________");
     }
 }
--- 9)Everything Is an Object/src/A9.java
+++ 9)Everything Is an Object/src/A9.java
@@ -2,9 +2,17 @@ public class A9 {
     public static void main(String[] args) {
         Integer a1 = new Integer(7);
         int a = a1;
+        long c = 5;
         Integer a2 = 9;
-        System.out.println(a1);
+        Integer a3 = (int)c;
+        Long b = (long)a2;
+        Long b2 = (long)a;
         System.out.println(a);
+        System.out.println(a1);
         System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(b2);
     }
 }
