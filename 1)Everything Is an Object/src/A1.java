97cd10c1f298c430d794241661a2c1b925ae2078
gromov-nikita <hromay.n999@gmail.com>
2020-11-14 17:41:06 +0300

    1

--- 1)Everything Is an Object/src/A1.java
+++ 1)Everything Is an Object/src/A1.java
@@ -1,6 +1,19 @@
 public class A1 {
     private int a;
     private char b;
+    public void setA(int a) {
+        this.a = a;
+    }
+    public void setB(char b) {
+        this.b = b;
+    }
+    public char getB() {
+        return b;
+    }
+    public int getA() {
+        return a;
+    }
+
     public static void main(String[] args) {
         A1 obj = new A1();
         System.out.println(obj.a);
